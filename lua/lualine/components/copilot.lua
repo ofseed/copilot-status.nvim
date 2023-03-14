2ddfa0ae07013dafff5904fd4c73c9ab4cdef765
Yi Ming <ofseed@foxmail.com>
2023-03-14 20:35:07 +0800

    refactor: rename and move `copilot_status` to `running_status`

--- lua/lualine/components/copilot.lua
+++ lua/lualine/components/copilot.lua
@@ -1,6 +1,6 @@
 local M = require("lualine.component"):extend()
 
----@alias CopilotStatus string
+---@alias CopilotRunningStatus string
 ---| '"running"' # copilot is running
 ---| '"idle"' # copilot is idle
 
@@ -21,19 +21,9 @@ local function is_enabled()
   end
 end
 
-function M:init(options)
-  M.super.init(self, options)
-  self.options = self.options or {}
-  self.inited = false
-  self.is_enabled = is_enabled
-  self.symbols = vim.tbl_deep_extend("keep", self.options.symbols or {}, symbols)
-  -- show copilot running status, default: true
-  self.show_running = self.options.show_running ~= false and true or false
-end
-
----Show copilot status
----@return CopilotStatus
-function M:copilot_status()
+---Show copilot running status
+---@return CopilotRunningStatus
+local function running_status()
   local agent = vim.g.loaded_copilot == 1 and vim.fn["copilot#RunningAgent"]() or nil
   if not agent then
     return "idle"
@@ -51,11 +41,21 @@ function M:copilot_status()
   return "idle"
 end
 
+function M:init(options)
+  M.super.init(self, options)
+  self.options = self.options or {}
+  self.inited = false
+  self.is_enabled = is_enabled
+  self.symbols = vim.tbl_deep_extend("keep", self.options.symbols or {}, symbols)
+  -- show copilot running status, default: true
+  self.show_running = self.options.show_running ~= false and true or false
+end
+
 function M:update_status()
   if self.inited then
     if self.is_enabled() then
       -- return symbols.enabled
-      local status = not self.show_running and "idle" or self:copilot_status()
+      local status = not self.show_running and "idle" or running_status()
       if status == "running" then
         return self.symbols.running
       end
